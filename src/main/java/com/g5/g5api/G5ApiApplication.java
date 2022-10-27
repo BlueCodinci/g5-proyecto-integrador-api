67ec85e8e688daf9aabcffb2f294e499b0aeb908
Leonardo Lopez Peve <leonardolopezpeve@hotmail.com>
2022-10-27 16:54:08 -0500

    Inicio de proyecto con configuraciones iniciales

---
+++ src/main/java/com/g5/g5api/G5ApiApplication.java
@@ -0,0 +1,13 @@
+package com.g5.g5api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G5ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G5ApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/g5/g5api/G5ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g5.g5api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G5ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
